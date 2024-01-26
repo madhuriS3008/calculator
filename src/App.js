0973c63ccd282ab805e0b4ec712bad49e8c5ecd6
Madhuri Sabbineni <madhusabbineni@gmail.com>
2024-01-26 09:52:44 -0600

    all functionality set for basic calculator

--- src/App.js
+++ src/App.js
@@ -12,23 +12,28 @@ function App() {
   }
 
   function minus(e) {
-    // Add the code for the minus function
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
   }
 
   function times(e) {
-    // Add the code for the plus function
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));
   }
 
   function divide(e) {
-    // Add the code for the divide function
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value));
   }
 
   function resetInput(e) {
-    // Add the code for the resetInput function
+    e.preventDefault();
+    inputRef.current.value = null;
   }
 
   function resetResult(e) {
-    // Add the code for the resetResult function
+    e.preventDefault();
+    setResult(0);
   }
 
   return (
@@ -37,7 +42,7 @@ function App() {
         <h1>Simplest Working Calculator</h1>
       </div>
       <form>
-        <p ref={resultRef}>{/* add the value of the current total */}</p>
+        <p ref={resultRef}>{result}</p>
         <input
           pattern="[0-9]"
           ref={inputRef}
@@ -46,10 +51,15 @@ function App() {
         />
         <button onClick={plus}>add</button>
         {/* Add the subtract button */}
+        <button onClick={minus}>subtract</button>
         {/* Add the multiply button */}
+        <button onClick={times}>multiply</button>
         {/* Add the divide button */}
+        <button onClick={divide}>divide</button>
         {/* Add the resetInput button */}
+        <button onClick={resetInput}>reset input</button>
         {/* Add the resetResult button */}
+        <button onClick={resetResult}>reset result</button>
       </form>
     </div>
   );
